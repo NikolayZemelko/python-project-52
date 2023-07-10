fa3997ae004184558e8ad36eec03c7988dcf1906
NikolayZemelko <zemelko1989@gmail.com>
2023-07-10 20:25:22 +0300

    feat: Placed i18n meta in method

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 from django.views import View
-from django.middleware import security
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
-class IndexView(View):
+def get_meta():
 
-    # i18n fields naming
-
-    def get(self, request, *args, **kwargs):
-
-        meta = dict(
+    return dict(
             Title=_('Менеджер задач Hexlet'),
             Project_name=_('Менеджер задач'),
             Users=_('Пользователи'),
@@ -20,10 +17,30 @@ class IndexView(View):
             Courses_name=_('Практические курсы по программированию'),
             About=_('Узнать больше'),
             Hexlet=_('Хекслет'),
+            Id=_('ID'),
+            UserName=_('Имя пользователя'),
+            FullName=_('Полное имя'),
+            DateOfCreate=_('Дата создания'),
+            Name=_('Имя'),
+            LastName=_('Фамилия'),
+            RequirmentField=_('Обязательное поле. Не более 150 символов.'
+                              ' Только буквы, цифры и символы'),
+            Password=_('Пароль'),
+            PasswordRequirment=_('Ваш пароль должен содержать как минимум 3 символа.'),
+            PasswordApproval=_('Подтверждение пароля'),
+            PasswordApprovalAgain=_('Для подтверждения введите, пожалуйста, пароль ещё раз.'),
+            Register=_('Зарегистрировать'),
         )
-        print(meta)
+
+
+class IndexView(View):
+
+    # i18n fields naming
+
+    def get(self, request, *args, **kwargs):
+
         return render(request, 'index.html', context={
-            'meta': meta,
+            'meta': get_meta(),
         })
 
 
@@ -31,21 +48,17 @@ class UsersView(View):
 
     def get(self, request, *args, **kwargs):
 
-        meta = dict(
-            Id=_('ID'),
-            UserName=_('Имя пользователя'),
-            FullName=_('Полное имя'),
-            DateOfCreate=_('Дата создания'),
-        )
         return render(request, 'users/index.html', context={
-            'meta': meta,
+            'meta': get_meta(),
         })
 
 
 class UsersCreateView(View):
 
     def get(self, request, *args, **kwargs):
-        ...
+        return render(request, 'users/create.html', context={
+            'meta': get_meta(),
+        })
 
     def post(self, request, *args, **kwargs):
         ...
@@ -54,7 +67,9 @@ class UsersCreateView(View):
 class UserUpdateView(View):
 
     def get(self, request, *args, **kwargs):
-        ...
+        return render(request, 'user/update.html', context={
+            'meta': get_meta(),
+        })
 
     def post(self, request, *args, **kwargs):
         ...
@@ -63,7 +78,9 @@ class UserUpdateView(View):
 class UserDeleteView(View):
 
     def get(self, request, *args, **kwargs):
-        ...
+        return render(request, 'user/delete.html', context={
+            'meta': get_meta(),
+        })
 
     def post(self, request, *args, **kwargs):
         ...
@@ -72,7 +89,9 @@ class UserDeleteView(View):
 class LoginView(View):
 
     def get(self, request, *args, **kwargs):
-        ...
+        return render(request, 'users/login.html', context={
+            'meta': get_meta(),
+        })
 
     def post(self, request, *args, **kwargs):
         ...
