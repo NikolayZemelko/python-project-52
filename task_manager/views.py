c340844279fc2f44798126b84a155335e4c47397
NikolayZemelko <zemelko1989@gmail.com>
2023-07-10 13:46:57 +0300

    feat: Add / view, add url / path

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.views import View
 
 
-def index(request):
+class IndexView(View):
 
-    greeting = 'Hello World!'
-    return render(request, 'index.html', context={
-        "greeting": greeting,
-    })
+    # i18n fields naming
+
+    def get(self, request, *args, **kwargs):
+
+        title = _('Менеджер задач Hexlet')
+        project_name = _('Менеджер задач')
+        users = _('Пользователи')
+        enter = _('Вход')
+        registration = _('Регистрация')
+        greetings = _('Привет от Хекслета!')
+        courses_name = _('Практические курсы по программированию')
+        about = _('Узнать больше')
+        hexlet = _('Хекслет')
+
+        return render(request, 'layout.html', context={
+            "title": title,
+            "project_name": project_name,
+            "users": users,
+            "enter": enter,
+            "registration": registration,
+            "greetings": greetings,
+            "courses_name": courses_name,
+            "about": about,
+            "hexlet": hexlet,
+        })
