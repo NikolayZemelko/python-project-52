fde1e04309296b4fa5f3bf3f7261a7b5f9956d78
NikolayZemelko <zemelko1989@gmail.com>
2023-07-17 20:46:50 +0300

    fix: Delete LoginForm to use Django default LoginView

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 class SignupForm(UserCreationForm):
@@ -7,11 +7,3 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'first_name', 'last_name')
-
-
-class SighInForm(AuthenticationForm):
-
-    class Meta:
-        model = get_user_model()
-        fields = ('username', 'password')
-
