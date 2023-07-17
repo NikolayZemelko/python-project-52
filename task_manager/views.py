3f548cd04085dccb271246b77c91431ea4a942d2
NikolayZemelko <zemelko1989@gmail.com>
2023-07-17 20:45:09 +0300

    feat: Rework CreateUserView, messages changed

--- task_manager/views.py
+++ task_manager/views.py
@@ -75,29 +75,29 @@ class UsersCreateView(View):
     def post(self, request, *args, **kwargs):
 
         form = SignupForm(request.POST)
+
         if form.is_valid():
             username = form.clean_username()
 
             if User.objects.filter(username=username):
                 messages.add_message(request, messages.ERROR,
-                                     'Пользователь с таким именем уже существует.')
+                                     _('Пользователь с таким именем уже существует.'))
                 return redirect('users-create', {
                     'form': form,
                 })
 
             else:
 
-                user = form.save(commit=False)
-                user.set_password(form.clean_password2())
-                user.save()
+                user = form.save()
                 login(request, user)
                 messages.add_message(request, messages.SUCCESS,
-                                     'Пользователь успешно зарегистрирован')
+                                     _('Пользователь успешно зарегистрирован'))
                 return redirect(settings.LOGIN_REDIRECT_URL)
 
         else:
             messages.add_message(request, messages.ERROR,
-                                 'Неправильные значения, попробуйте снова.')
+                                 _('Неправильные значения, попробуйте снова.'))
+
             return redirect('users-create')
 
 
