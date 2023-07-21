68602888b68715d8ef9e4ef3c513b0224820dafd
NikolayZemelko <zemelko1989@gmail.com>
2023-07-21 14:57:50 +0300

    feat: Change meta Values to English by default, add imports

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,52 +1,59 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.utils.translation import gettext as _
-from django.views import View
-from django.views.generic.edit import UpdateView, DeleteView
-from django.contrib.auth import login, logout
+from django.views.generic import UpdateView, DeleteView, CreateView, ListView
+from django.views.generic.base import TemplateView
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
 from task_manager.forms import SignupForm
 
 
 def get_meta():
 
     return dict(
-            Title=_('Менеджер задач Hexlet'),
-            Project_name=_('Менеджер задач'),
-            Users=_('Пользователи'),
-            EnterHeader=_('Вход'),
-            Entering=_('Войти'),
-            LogOut=_('Выход'),
-            Registration=_('Регистрация'),
-            Updating=_('Изменение пользователя'),
-            Greetings=_('Привет от Хекслета!'),
-            Courses_name=_('Практические курсы по программированию'),
-            About=_('Узнать больше'),
+            Title=_('Hexlet Task Manager'),
+            Project_name=_('Task Manager'),
+            Users=_('Users'),
+            EnterHeader=_('Log in'),
+            Entering=_('Sigh in'),
+            LogOut=_('Log Out'),
+            YouAreLogIn=_('You are logged in'),
+            YouAreLogOut=_('You are logged out'),
+            Registration=_('Registration'),
+            RegisteredSuccessfully=_('User has been registered successfully!'),
+            UpdatingSuccessfully=_('User has been updated successfully!'),
+            DelitedSuccessfully=_("User deleted successfully"),
+            DeleteButton=_('Yes, delete'),
+            NotAuthorised=_("You are not authorized! Please sign in."),
+            Updating=_('Change user'),
+            Greetings=_('Hello from Hexlet!'),
+            Courses_name=_('Practical programming courses'),
+            About=_('Learn more'),
             Hexlet=_('Hexlet'),
             Id=_('ID'),
-            UserName=_('Имя пользователя'),
-            FullName=_('Полное имя'),
-            DateOfCreate=_('Дата создания'),
-            Name=_('Имя'),
-            LastName=_('Фамилия'),
-            RequirmentField=_('Обязательное поле. Не более 150 символов.'
-                              ' Только буквы, цифры и символы'),
-            Password=_('Пароль'),
-            PasswordRequirment=_('Ваш пароль должен содержать как минимум 3 символа.'),
-            PasswordApproval=_('Подтверждение пароля'),
-            PasswordApprovalAgain=_('Для подтверждения введите, пожалуйста, пароль ещё раз.'),
-            Register=_('Зарегистрировать'),
-            UpdateUser=_('Изменить'),
-            DeleteUser=_('Удалить'),
-            DeletingUser=_('Удаление пользователя'),
-            DeletingApproving=_('Вы уверены, что хотите удалить'),
-            Statuses=_('Статусы'),
-            Labels=_('Метки'),
-            Tasks=_('Задачи'),
+            UserName=_('Username'),
+            FullName=_('Fullname'),
+            DateOfCreate=_('Date of creation'),
+            FirstName=_('Firstname'),
+            LastName=_('Lastname'),
+            RequirmentField=_('Obligatory field. No more than 150 characters. '
+                              'Only letters, numbers and symbols'),
+            Password=_('Password'),
+            PasswordRequirment=_('Your password must contain at least 3 characters.'),
+            PasswordApproval=_('Password confirmation'),
+            PasswordApprovalAgain=_('To confirm, please enter your password again.'),
+            Register=_('Register'),
+            UpdateUser=_('Change'),
+            DeleteUser=_('Delete'),
+            DeletingUser=_('Deleting a user'),
+            DeletingApproving=_('Are you sure you want to delete'),
+            Statuses=_('Statuses'),
+            Labels=_('Labels'),
+            Tasks=_('Tasks'),
         )
 
 
