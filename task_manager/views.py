6cca821bc45da404a81b326b6eae42149b7d733c
NikolayZemelko <zemelko1989@gmail.com>
2023-07-24 14:33:44 +0300

    feat: get_meta() moved to separate module

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,60 +1,13 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render, redirect
-from django.utils.translation import gettext as _
+from .meta import get_meta
 from django.views.generic import UpdateView, DeleteView, CreateView, ListView
 from django.views.generic.base import TemplateView
-from django.contrib.auth import logout
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from task_manager.forms import SignupForm
-
-
-def get_meta():
-
-    return dict(
-            Title=_('Hexlet Task Manager'),
-            Project_name=_('Task Manager'),
-            Users=_('Users'),
-            EnterHeader=_('Log in'),
-            Entering=_('Sigh in'),
-            LogOut=_('Log Out'),
-            YouAreLogIn=_('You are logged in'),
-            YouAreLogOut=_('You are logged out'),
-            Registration=_('Registration'),
-            RegisteredSuccessfully=_('User has been registered successfully!'),
-            UpdatingSuccessfully=_('User has been updated successfully!'),
-            DelitedSuccessfully=_("User deleted successfully"),
-            DeleteButton=_('Yes, delete'),
-            NotAuthorised=_("You are not authorized! Please sign in."),
-            Updating=_('Change user'),
-            Greetings=_('Hello from Hexlet!'),
-            Courses_name=_('Practical programming courses'),
-            About=_('Learn more'),
-            Hexlet=_('Hexlet'),
-            Id=_('ID'),
-            UserName=_('Username'),
-            FullName=_('Fullname'),
-            DateOfCreate=_('Date of creation'),
-            FirstName=_('Firstname'),
-            LastName=_('Lastname'),
-            RequirmentField=_('Obligatory field. No more than 150 characters. '
-                              'Only letters, numbers and symbols'),
-            Password=_('Password'),
-            PasswordRequirment=_('Your password must contain at least 3 characters.'),
-            PasswordApproval=_('Password confirmation'),
-            PasswordApprovalAgain=_('To confirm, please enter your password again.'),
-            Register=_('Register'),
-            UpdateUser=_('Change'),
-            DeleteUser=_('Delete'),
-            DeletingUser=_('Deleting a user'),
-            DeletingApproving=_('Are you sure you want to delete'),
-            Statuses=_('Statuses'),
-            Labels=_('Labels'),
-            Tasks=_('Tasks'),
-        )
+from task_manager.mixins import UserPermissionMixin, AuthRequiredMixin, DeleteProtectedMixin
 
 
 class IndexView(TemplateView):
