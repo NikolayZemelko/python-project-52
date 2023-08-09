f042f3dad42a69f891dae2eb47f8dcee996caebb
NikolayZemelko <zemelko1989@gmail.com>
2023-08-09 20:40:19 +0300

    feat: Add new concat-tag

--- task_manager/templatetags/task_manager_tags.py
+++ task_manager/templatetags/task_manager_tags.py
@@ -7,3 +7,8 @@ strformat = "%d.%m.%Y %H:%M"
 @register.simple_tag
 def date_format(datetime):
     return datetime.strftime(strformat)
+
+
+@register.simple_tag
+def concat(*fields):
+    return " ".join(fields) if fields else ""
