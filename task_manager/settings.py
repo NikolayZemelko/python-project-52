20d6b67cb24704f24d26e099241663e13fb8c86c
NikolayZemelko <zemelko1989@gmail.com>
2023-08-25 19:12:26 +0300

    feat: move HOSTS to env val

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
+                          default=['webserver', '127.0.0.1',
+                                   'localhost', '0.0.0.0'])
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
