90b5c2346942e2debf6785a0aa04a368a9a5ad14
NikolayZemelko <zemelko1989@gmail.com>
2023-08-25 20:32:50 +0300

    feat: Delete settings_dev.py

--- task_manager/settings_dev.py
+++
@@ -1,9 +0,0 @@
-from .settings import *  # noqa: F403
-
-
-DATABASES = {
-    'default': dj_database_url.config(  # noqa: F405
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-    )
-}
